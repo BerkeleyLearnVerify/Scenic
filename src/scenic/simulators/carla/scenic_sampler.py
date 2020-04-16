16e78cb347b85cee3d65db03ce2aa1a8d5da6689
Francis Indaheng <findaheng@berkeley.edu>
2020-04-16 14:31:44 -0700

    minor fix: renamed LGSVLSimulator to CarlaSimulator

--- src/scenic/simulators/carla/scenic_sampler.py
+++ src/scenic/simulators/carla/scenic_sampler.py
@@ -6,7 +6,7 @@ carla_world = 'Town01'
 sc_file_path = 'test.sc'
 ​
 # Create the Simulator
-simulator = LGSVLSimulator(carla_world, address='localhost', port=8181)
+simulator = CarlaSimulator(carla_world, address='localhost', port=8181)
 ​
 # Load Scenic scenario
 scenario = scenic.scenarioFromFile(sc_file_path)
