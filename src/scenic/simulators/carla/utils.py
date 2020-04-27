f264cc4f2270a5cf80f0182412775103a8255e0c
Francis Indaheng <findaheng@berkeley.edu>
2020-04-27 11:15:07 -0700

    minor syntax fix

--- src/scenic/simulators/carla/utils.py
+++ src/scenic/simulators/carla/utils.py
@@ -3,7 +3,7 @@ import math
 from scenic.core.vectors import Vector
 from scenic.core.geometry import normalizeAngle
 
-def snapToGround(self, world, location):
+def snapToGround(world, location):
 	"""Mutates @location to have the same z-coordinate as the nearest waypoint in @world."""
 	waypoint = self.world.get_map().get_waypoint(location)
 	location.z = waypoint.transform.location.z + 1
