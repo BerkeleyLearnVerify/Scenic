4a3b57bdb3cb6ff0a92fdfe6492a1085ac5c50f7
Francis Indaheng <findaheng@berkeley.edu>
2020-05-23 20:06:43 -0700

    refactored __main__

--- src/scenic/simulators/carla/__main__.py
+++ src/scenic/simulators/carla/__main__.py
@@ -3,29 +3,29 @@ import pygame
 
 from scenic.simulators.carla.simulator import CarlaSimulator
 
+# ==============================================================================
+# -- Parameters: CHANGEME ------------------------------------------------------
 
-# CHANGEME
 carla_map = 'Town01'
 sc_file_path = 'scripts/test.sc'
+
 address = '127.0.0.1'
 port = 2000
-render = True  # visualization mode ON/OFF
 
-# Create the Simulator
+render = True  # for HUD visualization
+
+numSimulations = 10
+maxSteps = 100
+
+# ==============================================================================
+
 simulator = CarlaSimulator(carla_map, address=address, port=port, render=render)
-
-# Load Scenic scenario
 scenario = scenic.scenarioFromFile(sc_file_path)
 
-# Compute number of time steps to run simulations
-timestep = 1.0 / 30
-maxSteps = 20.0 / timestep
-
-# Sample configurations from the scenario and run simulations
-itr = 0
-for _ in range(10):
+for _ in range(numSimulations):
     scene, __ = scenario.generate()
     simulation = simulator.createSimulation(scene)
     simulation.run(maxSteps)
     print('DONE')
+
 pygame.quit()
