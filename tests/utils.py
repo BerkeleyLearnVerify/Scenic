ef73e33f18368e809c66b230596cc5fb46ff318d
Daniel Fremont <dfremont@ucsc.edu>
2020-06-21 17:15:07 -0700

    run simulations from main script; fix random arguments to behaviors

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,14 @@
 
 from scenic import scenarioFromString as compileScenic
+from scenic.simulators.simulators import Simulator
 import scenic.syntax.veneer as veneer
 
 ## Scene generation utilities
 
-# Top level
+# Static scenes
+
+def sampleScene(scenario, maxIterations=1):
+    return generateChecked(scenario, maxIterations)[0]
 
 def sampleEgo(scenario, maxIterations=1):
     scene, iterations = generateChecked(scenario, maxIterations)
@@ -22,6 +26,25 @@ def sampleParamPFrom(code, maxIterations=1):
     scenario = compileScenic(code)
     return sampleParamP(scenario, maxIterations=maxIterations)
 
+# Dynamic simulations
+
+def sampleEgoActions(scenario, maxIterations=1, maxSteps=1):
+    scene, iterations = generateChecked(scenario, maxIterations)
+    return sampleEgoActionsFromScene(scene, maxIterations=maxIterations, maxSteps=maxSteps)
+
+def sampleEgoActionsFromScene(scene, maxIterations=1, maxSteps=1):
+    allActions = sampleActionsFromScene(scene, maxIterations=maxIterations, maxSteps=maxSteps)
+    return [actions[0] for actions in allActions]
+
+def sampleActions(scenario, maxIterations=1, maxSteps=1):
+    scene, iterations = generateChecked(scenario, maxIterations)
+    return sampleActionsFromScene(scene, maxIterations=maxIterations, maxSteps=maxSteps)
+
+def sampleActionsFromScene(scene, maxIterations=1, maxSteps=1):
+    sim = Simulator()
+    traj = sim.simulate(scene, maxSteps=maxSteps, maxIterations=maxIterations)
+    return traj[1:]
+
 # Helpers
 
 def generateChecked(scenario, maxIterations):
@@ -36,5 +59,8 @@ def checkVeneerIsInactive():
     assert len(veneer.allObjects) == 0
     assert veneer.egoObject is None
     assert len(veneer.globalParameters) == 0
+    assert len(veneer.externalParameters) == 0
     assert len(veneer.pendingRequirements) == 0
     assert len(veneer.inheritedReqs) == 0
+    assert len(veneer.behaviors) == 0
+    assert len(veneer.monitors) == 0
