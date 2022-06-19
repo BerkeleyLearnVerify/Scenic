9741915262ee4d1552b49273eecf8829f7c85a38
Shun <shunthedev@gmail.com>
2022-06-18 19:43:48 -0700

    Add grammar rule and AST for `new` and `ego =`

---
+++ tests/parser/test_ego_assign.py
@@ -0,0 +1,30 @@
+import ast
+import pytest
+
+from helper import parse_string_helper
+import scenic.ast as s
+
+@pytest.mark.match
+class TestEgoAssign:
+    def test_basic(self):
+        mod = parse_string_helper("ego = 10")
+        stmt = mod.body[0]
+        match stmt:
+            case s.EgoAssign(value=ast.Constant(value=v)):
+                assert v == 10
+            case _:
+                assert False
+    
+    def test_with_new(self):
+        mod = parse_string_helper("ego = new Object")
+        stmt = mod.body[0]
+        match stmt:
+            case s.EgoAssign(value=s.New(className=c)):
+                assert c == "Object"
+            case _:
+                assert False
+
+    def test_ego_keyword(self):
+        """Ego is a hard keyword and cannot be used except for ego assignment"""
+        with pytest.raises(SyntaxError):
+            parse_string_helper("ego, x = 10, 20")
