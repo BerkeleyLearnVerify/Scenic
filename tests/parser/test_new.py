e67599fa3ed24ef930d439666ce55fbe7825ff4e
Shun <shunthedev@gmail.com>
2022-06-19 20:18:16 -0700

    fix tests

--- tests/parser/test_new.py
+++ tests/parser/test_new.py
@@ -1,11 +1,8 @@
 import ast
-import pytest
 
-from helper import parse_string_helper
+from .helper import parse_string_helper
 import scenic.ast as s
 
-
-@pytest.mark.match
 class TestNew:
     def test_basic(self):
         mod = parse_string_helper("new Object")
