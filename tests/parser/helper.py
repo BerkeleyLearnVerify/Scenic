6e986d811c541515e601436d35e2d39ee7632e22
Shun <shunthedev@gmail.com>
2022-06-20 14:58:12 -0700

    move compiler tests and parser tests to `/tests/syntax`

--- tests/parser/helper.py
+++
@@ -1,6 +0,0 @@
-from typing import Any
-from scenic.syntax.parser import parse_string
-
-def parse_string_helper(source: str) -> Any:
-    "Parse string and return Scenic AST"
-    return parse_string(source, "exec")
