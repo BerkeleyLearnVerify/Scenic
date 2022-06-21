cc167d5bb9f5ee7c31322979c7c5477c33ce25f9
Shun <shunthedev@gmail.com>
2022-06-21 11:31:10 -0700

    implement `with` specifier

--- src/scenic/syntax/ast.py
+++ src/scenic/syntax/ast.py
@@ -12,9 +12,20 @@ class EgoAssign(AST):
         self.value = value
         self._fields = ["value"]
 
+# Instance Creation
+
 class New(AST):
     def __init__(self, className: str, specifiers: list, *args: any, **kwargs: any) -> None:
         super().__init__(*args, **kwargs)
         self.className = className
         self.specifiers = specifiers
         self._fields = ["className", "specifiers"]
+
+# Specifiers
+
+class WithSpecifier(AST):
+    def __init__(self, prop: str, value: ast.AST, *args: any, **kwargs: any) -> None:
+        super().__init__(*args, **kwargs)
+        self.prop = prop
+        self.value = value
+        self._fields = ["prop", "value"]
