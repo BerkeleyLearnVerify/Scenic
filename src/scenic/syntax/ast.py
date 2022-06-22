d6dc8c3dabdcc94c536847c10ee1839d185b2f56
Shun <shunthedev@gmail.com>
2022-06-21 22:08:44 -0700

    simplify parser tests

--- src/scenic/syntax/ast.py
+++ src/scenic/syntax/ast.py
@@ -1,36 +1,54 @@
 import ast
 
+
 class AST(ast.AST):
     "Scenic AST base class"
+
     def __init__(self, *args: any, **kwargs: any) -> None:
         super().__init__(*args, **kwargs)
 
+
 # special statements
 class EgoAssign(AST):
+    __match_args__ = ("value",)
+
     def __init__(self, value: any, *args: any, **kwargs: any) -> None:
         super().__init__(*args, **kwargs)
         self.value = value
         self._fields = ["value"]
 
+
 # Instance Creation
 
+
 class New(AST):
-    def __init__(self, className: str, specifiers: list, *args: any, **kwargs: any) -> None:
+    __match_args__ = ("className", "specifiers")
+
+    def __init__(
+        self, className: str, specifiers: list, *args: any, **kwargs: any
+    ) -> None:
         super().__init__(*args, **kwargs)
         self.className = className
         self.specifiers = specifiers
         self._fields = ["className", "specifiers"]
 
+
 # Specifiers
 
+
 class WithSpecifier(AST):
+    __match_args__ = ("prop", "value")
+
     def __init__(self, prop: str, value: ast.AST, *args: any, **kwargs: any) -> None:
         super().__init__(*args, **kwargs)
         self.prop = prop
         self.value = value
         self._fields = ["prop", "value"]
 
+
 class AtSpecifier(AST):
+    __match_args__ = ("position",)
+
     def __init__(self, position: ast.AST, *args: any, **kwargs: any) -> None:
         super().__init__(*args, **kwargs)
         self.position = position
