22534df43263b0a05a52c9aa80a1df0c94621287
Shun <shunthedev@gmail.com>
2022-06-21 23:41:35 -0700

    add offset by and offset along specifiers

--- src/scenic/syntax/compiler.py
+++ src/scenic/syntax/compiler.py
@@ -4,11 +4,13 @@ import scenic.syntax.ast as s
 
 # exposed functions
 
+
 def compileScenicAST(scenicAST: ast.AST) -> Tuple[ast.AST, List[ast.AST]]:
     """Compiles Scenic AST to Python AST"""
     compiler = ScenicToPythonTransformer()
     node = ast.fix_missing_locations(compiler.visit(scenicAST))
     return node, compiler.requirements
+
 
 # shorthands for convenience
 
@@ -16,24 +18,29 @@ loadCtx = ast.Load()
 
 # transformer
 
+
 class ScenicToPythonTransformer(ast.NodeTransformer):
     def __init__(self) -> None:
         super().__init__()
         self.requirements = []
-    
+
     def generic_visit(self, node):
         if isinstance(node, s.AST):
-            raise Exception(f'Scenic AST node "{node.__class__.__name__}" needs visitor in compiler')
+            raise Exception(
+                f'Scenic AST node "{node.__class__.__name__}" needs visitor in compiler'
+            )
         return super().generic_visit(node)
-    
+
     # Special Case
 
     def visit_EgoAssign(self, node: s.EgoAssign):
-        return ast.Expr(value=ast.Call(
-            func=ast.Name(id="ego", ctx=loadCtx),
-            args=[self.visit(node.value)],
-            keywords=[],
-        ))
+        return ast.Expr(
+            value=ast.Call(
+                func=ast.Name(id="ego", ctx=loadCtx),
+                args=[self.visit(node.value)],
+                keywords=[],
+            )
+        )
 
     # Instance & Specifier
 
@@ -58,5 +65,24 @@ class ScenicToPythonTransformer(ast.NodeTransformer):
         return ast.Call(
             func=ast.Name(id="At", ctx=loadCtx),
             args=[node.position],
+            keywords=[],
+        )
+
+    def visit_OffsetBySpecifier(self, node: s.OffsetBySpecifier):
+        return ast.Call(
+            func=ast.Name(id="OffsetBy", ctx=loadCtx),
+            args=[
+                self.visit(node.offset),
+            ],
+            keywords=[],
+        )
+
+    def visit_OffsetAlongSpecifier(self, node: s.OffsetAlongSpecifier):
+        return ast.Call(
+            func=ast.Name(id="OffsetAlongSpec", ctx=loadCtx),
+            args=[
+                self.visit(node.direction),
+                self.visit(node.offset),
+            ],
             keywords=[],
         )
