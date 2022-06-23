e69e2e7427200ae56b203cb6d982786b0ecf67a5
Shun <shunthedev@gmail.com>
2022-06-23 10:52:55 -0700

    rename to DirectionOfSpecifier

--- tests/syntax/test_compiler.py
+++ tests/syntax/test_compiler.py
@@ -90,7 +90,7 @@ class TestCompiler:
                 assert False
 
     def test_position_specifier_left(self):
-        node, _ = compileScenicAST(PositionSpecifier(LeftOf(), Name("x"), None))
+        node, _ = compileScenicAST(DirectionOfSpecifier(LeftOf(), Name("x"), None))
         match node:
             case Call(Name("LeftSpec"), [Name("x")]):
                 assert True
@@ -98,7 +98,7 @@ class TestCompiler:
                 assert False
 
     def test_position_specifier_right(self):
-        node, _ = compileScenicAST(PositionSpecifier(RightOf(), Name("x"), None))
+        node, _ = compileScenicAST(DirectionOfSpecifier(RightOf(), Name("x"), None))
         match node:
             case Call(Name("RightSpec"), [Name("x")]):
                 assert True
@@ -106,7 +106,7 @@ class TestCompiler:
                 assert False
 
     def test_position_specifier_ahead(self):
-        node, _ = compileScenicAST(PositionSpecifier(AheadOf(), Name("x"), None))
+        node, _ = compileScenicAST(DirectionOfSpecifier(AheadOf(), Name("x"), None))
         match node:
             case Call(Name("Ahead"), [Name("x")]):
                 assert True
@@ -114,7 +114,7 @@ class TestCompiler:
                 assert False
 
     def test_position_specifier_behind(self):
-        node, _ = compileScenicAST(PositionSpecifier(Behind(), Name("x"), None))
+        node, _ = compileScenicAST(DirectionOfSpecifier(Behind(), Name("x"), None))
         match node:
             case Call(Name("Behind"), [Name("x")]):
                 assert True
@@ -122,7 +122,7 @@ class TestCompiler:
                 assert False
 
     def test_position_specifier_distance(self):
-        node, _ = compileScenicAST(PositionSpecifier(Behind(), Name("x"), Constant(10)))
+        node, _ = compileScenicAST(DirectionOfSpecifier(Behind(), Name("x"), Constant(10)))
         match node:
             case Call(Name("Behind"), [Name("x")], [keyword("dist", Constant(10))]):
                 assert True
@@ -131,7 +131,7 @@ class TestCompiler:
 
     def test_position_specifier_unknown_direction(self):
         with pytest.raises(TypeError) as excinfo:
-            compileScenicAST(PositionSpecifier(str(), Name("x"), None))
+            compileScenicAST(DirectionOfSpecifier(str(), Name("x"), None))
         assert '"str" cannot be used as a direction' in str(excinfo.value)
 
     def test_visible_specifier(self):
