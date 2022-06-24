ee1a4c70d72b50ce89eb8b858bf7d2b836d87563
Shun <shunthedev@gmail.com>
2022-06-24 10:17:57 -0700

    add `relative to` / `offset by` operator

--- src/scenic/syntax/ast.py
+++ src/scenic/syntax/ast.py
@@ -421,3 +421,14 @@ class FieldAtOp(AST):
         super().__init__(*args, **kwargs)
         self.left = left
         self.right = right
+
+
+class RelativeToOp(AST):
+    __match_args__ = ("left", "right")
+
+    def __init__(
+        self, left: ast.AST, right: ast.AST, *args: any, **kwargs: any
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.left = left
+        self.right = right
