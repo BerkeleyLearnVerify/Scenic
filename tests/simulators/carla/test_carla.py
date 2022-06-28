360898f0c9b0d4a3071f1530c274358d92593a5d
Mirco Theile <mirco.theile@tum.de>
2022-06-28 11:09:39 -0700

    reverted test file

--- tests/simulators/carla/test_carla.py
+++ tests/simulators/carla/test_carla.py
@@ -1,12 +1,3 @@
-
 def test_basic(loadLocalScenario):
     scenario = loadLocalScenario('basic.scenic')
     scenario.generate(maxIterations=1000)
-
-
-# def test_simulator(loadLocalScenario):
-#     scenario = loadLocalScenario('basic.scenic')
-#     scene, _ = scenario.generate(maxIterations=1000)
-#     simulator = scenario.getSimulator()
-#     simulator.render = False
-#     simulator.createSimulation(scene)
