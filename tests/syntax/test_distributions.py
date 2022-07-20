cb3263f2c3408501b4df75369eb5b371fd955cc6
Shun Kashiwa <shunthedev@gmail.com>
2022-07-20 13:35:44 -0700

    fix syntax of `test_distributions`

--- tests/syntax/test_distributions.py
+++ tests/syntax/test_distributions.py
@@ -16,7 +16,7 @@ def lazyTestScenario(expr, offset='0'):
     return compileScenic(
         'vf = VectorField("Foo", lambda pos: 2 * pos.x)\n'
         f'x = {offset} relative to vf\n'
-        f'ego = Object at 0.5 @ 0, facing {expr}'
+        f'ego = new Object at 0.5 @ 0, facing {expr}'
     )
 
 ## Options and Uniform
@@ -46,14 +46,14 @@ def test_uniform_interval_wrong_type():
         compileScenic('x = Range(-10, [])')
 
 def test_uniform_interval():
-    scenario = compileScenic('ego = Object at Range(100, 200) @ 0')
+    scenario = compileScenic('ego = new Object at Range(100, 200) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(60)]
     assert all(100 <= x <= 200 for x in xs)
     assert any(x < 150 for x in xs)
     assert any(150 < x for x in xs)
 
 def test_uniform_discrete():
-    scenario = compileScenic('ego = Object at Uniform(1, 2, 3.4) @ 0')
+    scenario = compileScenic('ego = new Object at Uniform(1, 2, 3.4) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(100)]
     assert all(x == 1 or x == 2 or x == 3.4 for x in xs)
     assert any(x == 1 for x in xs)
@@ -69,7 +69,7 @@ def test_uniform_discrete_lazy():
 
 @pytest.mark.parametrize('dist', ('Options', 'Discrete'))
 def test_options(dist):
-    scenario = compileScenic(f'ego = Object at {dist}({{0: 1, 1: 9}}) @ 0')
+    scenario = compileScenic(f'ego = new Object at {dist}({{0: 1, 1: 9}}) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(200)]
     assert all(x == 0 or x == 1 for x in xs)
     assert 145 <= sum(xs) < 200
@@ -83,7 +83,7 @@ def test_options_lazy():
 ## Functions, methods, attributes, operators
 
 def test_function():
-    scenario = compileScenic('ego = Object at sin(Uniform(45 deg, 90 deg)) @ 0')
+    scenario = compileScenic('ego = new Object at sin(Uniform(45 deg, 90 deg)) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(60)]
     valA, valB = (pytest.approx(math.sin(math.radians(a))) for a in (45, 90))
     assert all(x == valA or x == valB for x in xs)
@@ -108,7 +108,7 @@ def test_method():
     scenario = compileScenic(
         'field = VectorField("Foo", lambda pos: pos[1])\n'
         'ang = field[0 @ Range(100, 200)]\n'
-        'ego = Object facing ang'
+        'ego = new Object facing ang'
     )
     angles = [sampleEgo(scenario).heading for i in range(60)]
     assert all(100 <= x <= 200 for x in angles)
@@ -125,7 +125,7 @@ def test_method_lazy():
             def bar(self, arg):
                 return -arg
         vf = VectorField("Baz", lambda pos: 1 + pos.x)
-        ego = Object facing Foo().bar(Range(100, 200) * (0 relative to vf))
+        ego = new Object facing Foo().bar(Range(100, 200) * (0 relative to vf))
     """)
     angles = [sampleEgo(scenario).heading for i in range(60)]
     assert all(-200 <= x <= -100 for x in angles)
@@ -141,7 +141,7 @@ def test_method_lazy_2():
         '    def bar(self, arg):\n'
         '        return -arg * Range(100, 200)\n'
         'vf = VectorField("Baz", lambda pos: 1 + pos.x)\n'
-        'ego = Object facing Foo().bar(0 relative to vf)'
+        'ego = new Object facing Foo().bar(0 relative to vf)'
     )
     angles = [sampleEgo(scenario).heading for i in range(60)]
     assert all(-200 <= x <= -100 for x in angles)
@@ -151,7 +151,7 @@ def test_method_lazy_2():
 def test_attribute():
     scenario = compileScenic(
         'place = Uniform(1 @ 1, 2 @ 4, 3 @ 9)\n'
-        'ego = Object at place.x @ place.y'
+        'ego = new Object at place.x @ place.y'
     )
     xs = [sampleEgo(scenario).position.x for i in range(100)]
     assert all(x == 1 or x == 2 or x == 3 for x in xs)
@@ -160,7 +160,7 @@ def test_attribute():
     assert any(x == 3 for x in xs)
 
 def test_operator():
-    scenario = compileScenic('ego = Object at -(100 + Range(0, 100)) @ 0')
+    scenario = compileScenic('ego = new Object at -(100 + Range(0, 100)) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(60)]
     assert all(-200 <= x <= -100 for x in xs)
     assert any(x < -150 for x in xs)
@@ -176,7 +176,7 @@ def test_operator_lazy():
 ## Vectors
 
 def test_vector_operator():
-    scenario = compileScenic('ego = Object at Range(-3, 3) @ 0 + Range(100, 110) @ 0')
+    scenario = compileScenic('ego = new Object at Range(-3, 3) @ 0 + Range(100, 110) @ 0')
     xs = [sampleEgo(scenario).position.x for i in range(100)]
     assert all(97 <= x <= 113 for x in xs)
     assert any(x < 105 for x in xs)
@@ -197,7 +197,7 @@ def test_vector_method_lazy_2():
 ## Lists, tuples, namedtuples
 
 def test_list():
-    scenario = compileScenic('ego = Object with foo [3, Uniform(1, 2)]')
+    scenario = compileScenic('ego = new Object with foo [3, Uniform(1, 2)]')
     ts = [sampleEgo(scenario).foo for i in range(60)]
     assert all(type(t) is list for t in ts)
     assert all(t[0] == 3 for t in ts)
@@ -206,7 +206,7 @@ def test_list():
     assert any(t[1] == 2 for t in ts)
 
 def test_list_param():
-    scenario = compileScenic('ego = Object\n' 'param p = [3, Uniform(1, 2)]')
+    scenario = compileScenic('ego = new Object\n' 'param p = [3, Uniform(1, 2)]')
     ts = [sampleParamP(scenario) for i in range(60)]
     assert all(type(t) is list for t in ts)
     assert all(t[0] == 3 for t in ts)
@@ -220,7 +220,7 @@ def test_list_param_lazy():
             'vf = VectorField("Foo", lambda pos: 2 * pos.x)\n'
             'x = 0 relative to vf\n'
             'param p = Uniform([0, x], [0, x*2])[1]\n'
-            'ego = Object'
+            'ego = new Object'
         )
 
 def test_list_object_lazy():
@@ -233,7 +233,7 @@ def test_list_object_lazy():
 def test_list_nested():
     scenario = compileScenic("""
         mylist = Uniform(list(range(1000)), [1000])
-        ego = Object with foo Uniform(*mylist)
+        ego = new Object with foo Uniform(*mylist)
     """)
     vs = [sampleEgo(scenario).foo for i in range(60)]
     assert 5 <= sum((v == 1000) for v in vs) <= 55
@@ -241,7 +241,7 @@ def test_list_nested():
 def test_list_nested_argument():
     scenario = compileScenic("""
         mylist = Uniform(list(range(1000)), [1, 1, 1, 1, 2000])
-        ego = Object with foo max(*mylist)
+        ego = new Object with foo max(*mylist)
     """)
     vs = [sampleEgo(scenario).foo for i in range(60)]
     assert 5 <= sum((v == 2000) for v in vs) <= 55
@@ -250,14 +250,14 @@ def test_list_filtered():
     scenario = compileScenic("""
         mylist = [Range(-10, -5), Range(3, 7), Range(-1, 1)]
         filtered = filter(lambda x: x > 0, mylist)
-        ego = Object with foo Uniform(*filtered)
+        ego = new Object with foo Uniform(*filtered)
     """)
     vs = [sampleEgo(scenario).foo for i in range(60)]
     assert all(v > 0 for v in vs)
     assert any(v < 1 for v in vs)
 
 def test_tuple():
-    scenario = compileScenic('ego = Object with foo tuple([3, Uniform(1, 2)])')
+    scenario = compileScenic('ego = new Object with foo tuple([3, Uniform(1, 2)])')
     ts = [sampleEgo(scenario).foo for i in range(60)]
     assert all(type(t) is tuple for t in ts)
     assert all(t[0] == 3 for t in ts)
@@ -266,7 +266,7 @@ def test_tuple():
     assert any(t[1] == 2 for t in ts)
 
 def test_tuple_param():
-    scenario = compileScenic('ego = Object\n' 'param p = tuple([3, Uniform(1, 2)])')
+    scenario = compileScenic('ego = new Object\n' 'param p = tuple([3, Uniform(1, 2)])')
     ts = [sampleParamP(scenario) for i in range(60)]
     assert all(type(t) is tuple for t in ts)
     assert all(t[0] == 3 for t in ts)
@@ -278,7 +278,7 @@ def test_namedtuple():
     scenario = compileScenic(
         'from collections import namedtuple\n'
         'Data = namedtuple("Data", ["bar", "baz"])\n'
-        'ego = Object with foo Data(bar=3, baz=Uniform(1, 2))'
+        'ego = new Object with foo Data(bar=3, baz=Uniform(1, 2))'
     )
     ts = [sampleEgo(scenario).foo for i in range(60)]
     assert all(t.bar == 3 for t in ts)
@@ -291,9 +291,9 @@ def test_namedtuple():
 @pytest.mark.slow
 def test_reproducibility():
     scenario = compileScenic(
-        'ego = Object\n'
-        'Object offset by 0@3, facing Range(0, 360) deg\n'
-        'Object offset by 0@6, facing Range(0, 360) deg\n'
+        'ego = new Object\n'
+        'new Object offset by 0@3, facing Range(0, 360) deg\n'
+        'new Object offset by 0@6, facing Range(0, 360) deg\n'
         'param foo = Uniform(1, 4, 9, 16, 25, 36)\n'
         'x = Range(0, 1)\n'
         'require x > 0.8'
@@ -314,7 +314,7 @@ def test_reproducibility():
 ## Independence
 
 def test_independence():
-    scenario = compileScenic('ego = Object at Range(0, 1) @ Range(0, 1)')
+    scenario = compileScenic('ego = new Object at Range(0, 1) @ Range(0, 1)')
     pos = sampleEgo(scenario).position
     assert pos.x != pos.y
 
@@ -323,7 +323,7 @@ def test_independence():
 def test_shared_dependency():
     scenario = compileScenic(
         'x = Range(-1, 1)\n'
-        'ego = Object at (x * x) @ 0'
+        'ego = new Object at (x * x) @ 0'
     )
     xs = [sampleEgo(scenario).position.x for i in range(60)]
     assert all(0 <= x <= 1 for x in xs)
@@ -335,7 +335,7 @@ def test_shared_dependency_lazy_1():
         vf = VectorField("Foo", lambda pos: 2 * pos.x)
         x = 1 relative to vf
         y = Uniform(0, x)
-        ego = Object with foo y, with bar y
+        ego = new Object with foo y, with bar y
     """)
     for i in range(60):
         ego = sampleEgo(scenario)
@@ -346,8 +346,8 @@ def test_shared_dependency_lazy_2():
     scenario = compileScenic(
         'vf = VectorField("Foo", lambda pos: 2 * pos.x)\n'
         'x = Range(0, 1) relative to vf\n'
-        'ego = Object at 1 @ 0, facing x\n'
-        'other = Object at -1 @ 0, facing x'
+        'ego = new Object at 1 @ 0, facing x\n'
+        'other = new Object at -1 @ 0, facing x'
     )
     for i in range(60):
         scene = sampleScene(scenario, maxIterations=1)
@@ -369,8 +369,8 @@ def test_inside_delayed_argument():
 
 def test_object_expression():
     scenario = compileScenic("""
-        v = Uniform((Object at Range(-2,-1) @ 0), Object at Range(1,2) @ 5).position.x
-        ego = Object facing v, at 0 @ 10
+        v = Uniform((new Object at Range(-2,-1) @ 0), new Object at Range(1,2) @ 5).position.x
+        ego = new Object facing v, at 0 @ 10
         require abs(v) > 1.5
     """)
     for i in range(3):
