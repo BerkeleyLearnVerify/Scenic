b46aac34283aa8d16af47457b1def6db588d8918
Shun Kashiwa <shunthedev@gmail.com>
2022-07-22 14:59:54 -0700

    fix test_regions

--- tests/syntax/test_regions.py
+++ tests/syntax/test_regions.py
@@ -11,12 +11,12 @@ from tests.utils import compileScenic, sampleScene, sampleEgo, sampleEgoFrom
 # Built-in regions
 
 def test_everywhere():
-    scenario = compileScenic('ego = Object with regionContainedIn everywhere')
+    scenario = compileScenic('ego = new Object with regionContainedIn everywhere')
     sampleScene(scenario, maxIterations=1)
 
 def test_nowhere():
     with pytest.raises(InvalidScenarioError):
-        compileScenic('ego = Object with regionContainedIn nowhere')
+        compileScenic('ego = new Object with regionContainedIn nowhere')
 
 # CircularRegion
 
@@ -44,8 +44,8 @@ def test_circular_lazy():
 def test_polygonal_empty_intersection():
     scenario = compileScenic("""
         r1 = PolygonalRegion([0@0, 10@0, 10@10, 0@10])
-        ego = Object at -10@0, facing Range(-90, 0) deg, with viewAngle 60 deg
-        Object in visible r1, with requireVisible False
+        ego = new Object at -10@0, facing Range(-90, 0) deg, with viewAngle 60 deg
+        new Object in visible r1, with requireVisible False
     """)
     for i in range(30):
         sampleScene(scenario, maxIterations=1000)
@@ -56,7 +56,7 @@ def test_polyline_start():
     ego = sampleEgoFrom("""
         r = PolylineRegion([1@1, 3@-1, 6@2])
         pt = r.start
-        ego = Object at pt, facing pt.heading
+        ego = new Object at pt, facing pt.heading
     """)
     assert tuple(ego.position) == pytest.approx((1, 1))
     assert ego.heading == pytest.approx(math.radians(-135))
@@ -65,7 +65,7 @@ def test_polyline_end():
     ego = sampleEgoFrom("""
         r = PolylineRegion([1@1, 3@-1, 6@2])
         pt = r.end
-        ego = Object at pt, facing pt.heading
+        ego = new Object at pt, facing pt.heading
     """)
     assert tuple(ego.position) == pytest.approx((6, 2))
     assert ego.heading == pytest.approx(math.radians(-45))
