5a396033b99ee48ab9d48e06f3c2b46ef7cd04e9
Shun Kashiwa <shunthedev@gmail.com>
2022-08-18 18:41:10 +0900

    implement `abort` statements

--- src/scenic/syntax/compiler.py
+++ src/scenic/syntax/compiler.py
@@ -515,6 +515,16 @@ class ScenicToPythonTransformer(ast.NodeTransformer):
             )
         )
 
+    def visit_Abort(self, node: s.Abort):
+        return ast.copy_location(
+            ast.Return(
+                ast.Attribute(
+                    ast.Name("BlockConclusion", ast.Load()), "ABORT", ast.Load()
+                )
+            ),
+            node,
+        )
+
     # Instance & Specifier
 
     def visit_New(self, node: s.New):
