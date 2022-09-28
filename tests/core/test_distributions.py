2f1463bc082a5619ff435decf7ed9d5d5fbfa952
Daniel Fremont <dfremont@ucsc.edu>
2022-09-28 11:10:43 -0700

    adjust statistical tests to reduce spurious failures

--- tests/core/test_distributions.py
+++ tests/core/test_distributions.py
@@ -12,38 +12,44 @@ from scenic.core.distributions import (Range, Normal, TruncatedNormal, DiscreteR
 
 # Bucketing
 
-def similarDistributions(d1, d2, samples=3000, p=0.002):
-    s1 = [d1.sample() for i in range(samples)]
-    s2 = [d2.sample() for i in range(samples)]
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        try:
-            assert scipy.stats.epps_singleton_2samp(s1, s2).pvalue > p, p
-        except numpy.linalg.LinAlgError:
-            assert scipy.stats.ks_2samp(s1, s2).pvalue > p, p
+@pytest.fixture
+def similarDistributions(pytestconfig):
+    samples = 3000 if pytestconfig.getoption('--fast') else 100000
 
-def test_bucketed_range():
+    def checker(d1, d2, p=1e-4):
+        s1 = [d1.sample() for i in range(samples)]
+        s2 = [d2.sample() for i in range(samples)]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            try:
+                assert scipy.stats.epps_singleton_2samp(s1, s2).pvalue > p, p
+            except numpy.linalg.LinAlgError:
+                assert scipy.stats.ks_2samp(s1, s2).pvalue > p, p
+
+    return checker
+
+def test_bucketed_range(similarDistributions):
     r = Range(-3, 7)
     similarDistributions(r, r.bucket())
     similarDistributions(r, r.bucket(buckets=100))
 
-def test_bucketed_normal():
+def test_bucketed_normal(similarDistributions):
     n = Normal(22, 5)
     similarDistributions(n, n.bucket())
     similarDistributions(n, n.bucket(buckets=15))
     similarDistributions(n, n.bucket(buckets=[0, 20, 23, 50]))
 
-def test_bucketed_truncated_normal():
+def test_bucketed_truncated_normal(similarDistributions):
     n = TruncatedNormal(-10, 3, -1, 5)
     similarDistributions(n, n.bucket())
     similarDistributions(n, n.bucket(buckets=15))
     similarDistributions(n, n.bucket(buckets=[-1, -0.5, 4, 5]))
 
-def test_bucketed_discrete_range():
+def test_bucketed_discrete_range(similarDistributions):
     r = DiscreteRange(-3, 7)
     similarDistributions(r, r.bucket())
 
-def test_bucketed_options():
+def test_bucketed_options(similarDistributions):
     o = Options({0: 1, 1: 3})
     similarDistributions(o, o.bucket())
 
