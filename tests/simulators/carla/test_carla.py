7ec3d947d17311d4196cdece7956d1cae3fd470b
Daniel Fremont <dfremont@ucsc.edu>
2023-08-31 17:26:52 -0700

    fix inconsistent CARLA model import issue

--- tests/simulators/carla/test_carla.py
+++ tests/simulators/carla/test_carla.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.utils import pickle_test, sampleScene, tryPickling
+from tests.utils import compileScenic, pickle_test, sampleScene, tryPickling
 
 # Suppress potential warning about missing the carla package
 pytestmark = pytest.mark.filterwarnings(
@@ -16,6 +16,21 @@ def test_basic(loadLocalScenario):
 def test_simulator_import():
     pytest.importorskip("carla")
     from scenic.simulators.carla import CarlaSimulator
+
+
+def test_consistent_object_type(getAssetPath):
+    pytest.importorskip("carla")
+    mapPath = getAssetPath("maps/CARLA/Town01.xodr")
+    code = rf"""
+        param map = '{mapPath}'
+        param carla_map = 'Town01'
+        model scenic.simulators.carla.model
+        action = SetGearAction(0)
+        ego = new Car
+        assert action.canBeTakenBy(ego)
+    """
+    for _ in range(2):
+        compileScenic(code, mode2D=True)
 
 
 @pickle_test
