0d9b7419506d7e57da5deda569d49a4633b218cd
Eric Vin <evin@ucsc.edu>
2023-10-04 14:06:33 -0700

    Re-enabled opendrive tests (still issue with xfail)

--- tests/formats/opendrive/test_opendrive.py
+++ tests/formats/opendrive/test_opendrive.py
@@ -7,26 +7,20 @@ import pytest
 
 from scenic.core.geometry import TriangulationError
 from scenic.formats.opendrive import OpenDriveWorkspace
-
-oldDir = os.getcwd()
-os.chdir(Path("tests") / "formats" / "opendrive")
-mapPath = Path("maps") / "**" / "*.xodr"
-maps = glob.glob(str(mapPath))
-os.chdir(oldDir)
+from tests.domains.driving.conftest import map_params
 
 
 @pytest.mark.slow
+@pytest.mark.parametrize("path", map_params)
 @pytest.mark.filterwarnings("ignore::scenic.formats.opendrive.OpenDriveWarning")
-@pytest.mark.parametrize("path", maps)
-def test_map(path, runLocally, pytestconfig):
-    with runLocally():
-        try:
-            odw = OpenDriveWorkspace(path, n=10)
-        except TriangulationError:
-            pytest.skip("need better triangulation library to run this test")
-        pt = odw.drivable_region.uniformPointInner()
-        odw.road_direction[pt]
-        if not pytestconfig.getoption("--no-graphics"):
-            odw.show(plt)
-            plt.show(block=False)
-            plt.close()
+def test_map(path, pytestconfig):
+    try:
+        odw = OpenDriveWorkspace(path, n=10)
+    except TriangulationError:
+        pytest.skip("need better triangulation library to run this test")
+    pt = odw.drivable_region.uniformPointInner()
+    odw.road_direction[pt]
+    if not pytestconfig.getoption("--no-graphics"):
+        odw.show(plt)
+        plt.show(block=False)
+        plt.close()
