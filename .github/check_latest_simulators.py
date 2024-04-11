f0a21acb2b1d3626f73416d65410ddda22702297
Armando Banuelos <abanuelo@Armandos-MBP.lan>
2024-04-11 13:42:50 -0700

    fix: reformatting file to check for latest sims with black and isort

--- .github/check_latest_simulators.py
+++ .github/check_latest_simulators.py
@@ -1,6 +1,8 @@
 import os
-import requests
 import re
+
+import requests
+
 
 def check_path_exists(version, simulator):
     path = f"/software/{simulator}{version}"
@@ -15,12 +17,17 @@ def check_path_exists(version, simulator):
                            /___/  
         """
         print(s)
-        print(f"A new {simulator} version ({version}) needs to be installed and tested in CI.\n")
+        print(
+            f"A new {simulator} version ({version}) needs to be installed and tested in CI.\n"
+        )
+
 
 print("Checking for CARLA...")
-carla_url = 'https://github.com/carla-simulator/carla/releases/latest'
+carla_url = "https://github.com/carla-simulator/carla/releases/latest"
 carla_response = requests.get(carla_url)
-carla_match = re.search(r'carla-simulator/carla/releases/tag/([^"]+)', carla_response.text)
+carla_match = re.search(
+    r'carla-simulator/carla/releases/tag/([^"]+)', carla_response.text
+)
 
 try:
     version = carla_match.group(1)
@@ -29,12 +36,12 @@ except AttributeError:
     print("Error: Unable to find the latest CARLA version using regex.")
 
 print("Checking for Webots...")
-webots_url = 'https://github.com/cyberbotics/webots/releases/latest'
+webots_url = "https://github.com/cyberbotics/webots/releases/latest"
 webots_response = requests.get(webots_url)
 webots_match = re.search(r'cyberbotics/webots/releases/tag/([^"]+)', webots_response.text)
 
 try:
     version = webots_match.group(1)
-    check_path_exists(version, 'webots')
+    check_path_exists(version, "webots")
 except AttributeError:
     print("Error: Unable to find the latest Webots version using regex.")
