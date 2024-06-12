fe3945e47d5b88f6b91f862516768137809091df
Kai Xu <kxu@lav.EECS.Berkeley.EDU>
2024-06-12 01:28:24 -0700

    finished actions.py

--- src/scenic/simulators/template_interface/actions.py
+++ src/scenic/simulators/template_interface/actions.py
@@ -1,35 +1,157 @@
-import math
-import sys
+"""
+Introduction to Scenic Actions
 
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-import controller_manager_msgs.srv
-from geometry_msgs.msg import (
-    Point,
-    Pose,
-    PoseStamped,
-    Quaternion,
-    TransformStamped,
-    Twist,
-)
-import numpy as np
-import rospy
-import tf2_ros
 
-# import tf.transformations as T
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
-import trajectory_msgs.msg
-from visualization_msgs.msg import Marker, MarkerArray
+This file, actions.py, is where you should define all
+your Scenic Actions that are to be carried out with the 
+'take <Action>' syntax. 
 
-from scenic.core.simulators import *
-from scenic.simulators.Gazebo.utils.state_utils import ApplyROSTransform
+Each Action should be a class inheriting from
+the Action class from scenic.core.simulators
+"""
+
+from scenic.core.simulators import * # imports the Action superclass
+
+
+class ExampleAction1(Action):
+    """
+    Each Action has two methods you need to implement,
+    __init__() and the applyTo(). We will see how to implement
+    them below
+    """
+
+    def __init__(self):
+        """
+        __init__ can have any arguments beside self based on the need of your actions
+        for instance, you can have
+
+        def __init__(self, x, y, z)
+
+        as the funciton signature, and when taking the Action in your Scenic program,
+        you would write:
+
+        take ExampleAction1(x, y, z)
+
+        You should not be execute or send the command
+        for the action in __init__. That is the applyTo method's job.
+
+        Other than that, there is no requirement as to what to do in __init__. You can
+        fill in the code according to your needs
+        """
+
+        pass
+
+
+    def applyTo(self, obj, sim):
+        """
+        Contrary to __init__(), applyTo()'s signature is fixed
+        to applyTo(self, obj, sim). You should not change this.
+        
+        Args:
+        Object obj: The Scenic object that takes the action
+        scenic.core.simulator.Simulation sim: the Simulation instance you implemented in simulator.py
+
+        applyTo is generally where you send the command to the simulator to execute an action
+
+        You can choose to let it return anything or None based on your needs. However,
+        remember that your implementation for the step() and executeAction() in simulator.py
+        to account for this
+
+        """
+        pass
+
+
 
 """
-READ:
-This is the library of all Actions. For each any actions you would like to perform,
-check if a corresponding behavior exists in behavior.scenic. It is recommended to use
-the behaviors since those take care of waiting for controllers and waiting until finish
-
-If you would like to add a new Action, make sure the applyTo() function returns a function
-that takes no arguments
+Let's see an example Actions
 """
+
+class MoveAction(Action):
+    """
+    This Action sends to command to move the agent
+    to an (x, y, z) coordinate
+    """
+
+    def __init__(self, x=0, y=0, z=0):
+        self.target_coord = [x, y, z]
+
+    def applyTo(self, obj, sim):
+
+        agent_id = obj.agent_id
+
+        target_reachable = sim.YourSimulatorAPI.target_reachable(self.target_coord)
+
+        if target_reachable:
+            YourSimulatorAPI.move_agent(agent_id=agent_id, target_coordinate=self.target_coord)
+
+
+
+"""
+Here's an example where applyTo returns a function that executes a command
+"""
+
+class MoveAction(Action):
+    """
+    This Action sends to command to move the agent
+    to an (x, y, z) coordinate
+    """
+
+    def __init__(self, x=0, y=0, z=0):
+        self.target_coord = [x, y, z]
+
+    def applyTo(self, obj, sim):
+        agent_id = obj.agent_id
+
+        target_reachable = sim.YourSimulatorAPI.target_reachable(self.target_coord)
+
+        if target_reachable:
+            f = lambda: sim.YourSimulatorAPI.move_agent(agent_id=agent_id, target_coordinate=self.target_coord)
+
+        return f
+
+
+
+"""
+An example for a BAD Action:
+If for some reason, you cannot pause the simulator between calls of the step() method
+in simulator.py, you should no have any code in Action that blocks code execution
+
+This prevents Scenic from simultaneously (at least approximately simulatenaously) manage 
+all the agents, objects, and simulation world.
+"""
+
+class BadAction(Action):
+    """
+    This Action sends to command to move the agent
+    to an (x, y, z) coordinate and waits for it to reach its goal
+    """
+
+    def __init__(self, x=0, y=0, z=0):
+        self.target_coord = [x, y, z]
+
+    def applyTo(self, obj, sim):
+        """
+        Here we send a command to move an agent and then wait until
+        the agent reaches its goal using the final while loop.
+
+        This will cause problems as Scenic is prevented from
+        managing and sending the action commands for other agents.
+        Scenic would also not be able to get information of the simulation world
+        during the Time when the agent is moving.
+        
+
+        This kind of blocking code can be common in robot API's, where 
+        methods that commands the robot often conatins code that waits
+        until the robot has finished executing its actions.
+
+        We will see a way around this problem in behaviors.scenic
+        """
+
+        agent_id = obj.agent_id
+
+        YourSimulatorAPI.move_agent(agent_id=agent_id, target_coordinate=self.target_coord)
+
+        while not YourSimulatorAPI.agent_reached_goal(agent_id):
+            continue
+
+
