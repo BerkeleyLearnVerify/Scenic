8a2a7e9a607bd9732f69ab80d9db077d9dca810c
Armando Bañuelos <32311654+abanuelo@users.noreply.github.com>
2024-12-20 15:31:23 -0800

    feat: adding webots dynamic scenario tests (#231)
    * feat: adding webots dynamic simulation, tbd tested
    
    * feat: Adding functional webots tests
    
    * fix: reformatting with black and isort and deleting unnecessary files
    
    * fix: improving code coverage
    
    * fix: black formatting test_webots.py
    
    * Adding codecov yml updates from main branch
    
    * fix: also including tests directory to ignore for coverage, not relevant
    
    * fix: adding checks for environment variables
    
    * fix: adjusting fixture call
    
    * fix: adding webots extension
    
    * fix: adding batch option to avoid popups
    
    * Add timeout to Webots dynamic test
    
    ---------
    
    Co-authored-by: Armando Bañuelos <abanuelo@trurl.soe.ucsc.edu>
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-23-163.us-west-1.compute.internal>
    Co-authored-by: Armando Banuelos <abanuelo@Armandos-MBP.lan>
    Co-authored-by: Armando Banuelos <abanuelo@Armandos-MacBook-Pro.local>
    Co-authored-by: Lola Marrero <110120745+lola831@users.noreply.github.com>

--- tests/simulators/webots/test_webots.py
+++ tests/simulators/webots/test_webots.py
@@ -1,6 +1,49 @@
+import os
+import subprocess
+
 import pytest
 
 from tests.utils import pickle_test, sampleScene, tryPickling
+
+WEBOTS_RESULTS_FILE_PATH = f"{os.path.dirname(__file__)}/dynamic/results.txt"
+WEBOTS_WORLD_FILE_PATH = (
+    f"{os.path.dirname(__file__)}/dynamic/webots_data/worlds/world.wbt"
+)
+
+
+def receive_results():
+    with open(WEBOTS_RESULTS_FILE_PATH, "r") as file:
+        content = file.read()
+    return content
+
+
+def cleanup_results():
+    command = f"rm -f {WEBOTS_RESULTS_FILE_PATH}"
+    subprocess.run(command, shell=True)
+
+
+def test_dynamics_scenarios(launchWebots):
+    WEBOTS_ROOT = launchWebots
+    cleanup_results()
+
+    timeout_seconds = 300
+    
+    command = f"bash {WEBOTS_ROOT}/webots --no-rendering --minimize --batch {WEBOTS_WORLD_FILE_PATH}"
+
+    try:
+        subprocess.run(command, shell=True, timeout=timeout_seconds)
+    except subprocess.TimeoutExpired:
+        pytest.fail(f"Webots test exceeded the timeout of {timeout_seconds} seconds and failed.")
+        
+    data = receive_results()
+    assert data != None
+    start_z = float(data.split(",")[1].strip(" )]"))
+    end_z = float(data.split(",")[3].strip(" )]"))
+    assert start_z == 0.5
+    assert start_z > end_z
+    expected_value = 0.09
+    tolerance = 0.01
+    assert end_z == pytest.approx(expected_value, abs=tolerance)
 
 
 def test_basic(loadLocalScenario):
