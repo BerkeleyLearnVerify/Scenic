e3d09a165d497618fbeba9dd9f3b99bf3bb097c8
Kai Xu <k.px@berkeley.edu>
2025-03-21 17:29:36 -0700

    temporarily commented out the OAI15 stuff

--- src/scenic/gym/__init__.py
+++ src/scenic/gym/__init__.py
@@ -1,5 +1,5 @@
 from scenic.gym.envs.scenic_gym import ScenicGymEnv
-from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
-from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
+# from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
+# from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
 from gym import register
 
--- src/scenic/gym/envs/__init__.py
+++ src/scenic/gym/envs/__init__.py
@@ -1,3 +1,3 @@
 from scenic.gym.envs.scenic_gym import ScenicGymEnv
-from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
-from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
+# from scenic.gym.envs.scenic_oai_15_gym import ScenicOAI15GymEnv
+# from scenic.gym.envs.scenic_oai_gym import ScenicOAIGymEnv
