356686a1dcc98a4a7ffd2efaf230358e28e2bdbe
lola <keepthechange2021@gmail.com>
2025-12-17 16:39:49 -0800

    Address PR review comments

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -35,13 +35,17 @@ def _check_intersphinx_connectivity(timeout=10.0):
 
 @pytest.mark.slow
 def test_build_docs():
-    """Test that the documentation builds and doctests pass."""
+    """Test that the documentation builds, and run doctests.
+
+    We do this in a subprocess since the Sphinx configuration file activates the veneer
+    and has other side-effects that aren't reset afterward.
+    """
     _check_intersphinx_connectivity()
 
-    old_directory = os.getcwd()
+    oldDirectory = os.getcwd()
     try:
         os.chdir("docs")
         subprocess.run(["make", "clean", "html", "SPHINXOPTS=-W"], check=True)
         subprocess.run(["make", "doctest", "SPHINXOPTS=-W"], check=True)
     finally:
-        os.chdir(old_directory)
+        os.chdir(oldDirectory)
