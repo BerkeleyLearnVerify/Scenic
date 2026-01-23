bbee6434849ee2fe941faf994d2a0fcf0690507d
Lola Marrero <lmarrero@ucsc.edu>
2026-01-23 09:24:24 -0800

    bump .snet format version

--- src/scenic/domains/driving/roads.py
+++ src/scenic/domains/driving/roads.py
@@ -1196,7 +1196,7 @@ class Network:
 
         :meta private:
         """
-        return 34
+        return 35
 
     class DigestMismatchError(Exception):
         """Exception raised when loading a cached map not matching the original file."""
