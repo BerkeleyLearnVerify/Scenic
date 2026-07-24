b954a43c199aae4840facb4f826c43731fc2e81e
Eric Vin <evin@ucsc.edu>
2026-07-24 15:10:54 -0700

    Attempt fix.

--- src/scenic/core/external_params/optuna.py
+++ src/scenic/core/external_params/optuna.py
@@ -104,14 +104,16 @@ class OptunaParameterConverter(ExternalParameterConverter):
             newDist = OptunaOptions(distCond.options)
             self.registerExternalParams(newDist)
             dist.conditionTo(newDist)
-        # elif isinstance(distCond, UniformDistribution):
-        #     for o in distCond.options:
-        #         self.convert(o)
-        #     newSelector = _OptunaCategoricalHelper(
-        #         distCond.selector.high, emptyMessage="Empty Optuna UniformDistribution."
-        #     )
-        #     self.registerExternalParams(newSelector)
-        #     distCond.selector.conditionTo(newSelector)
+        elif isinstance(distCond, UniformDistribution):
+            for o in distCond.options:
+                self.convert(o)
+            newSelector = OptunaDiscreteRange(
+                0,
+                distCond.selector.high,
+                emptyMessage="Empty Optuna UniformDistribution.",
+            )
+            self.registerExternalParams(newSelector)
+            distCond.selector.conditionTo(newSelector)
         elif (
             isinstance(distCond, PointInRegionDistribution)
             and not distCond._deterministic
