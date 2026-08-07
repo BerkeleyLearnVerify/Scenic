a6cd81f67270c8c6e6e60026e1dbbb0432b286ae
Eric Vin <evin@ucsc.edu>
2026-08-07 16:11:11 -0700

    Debug

--- src/scenic/syntax/veneer.py
+++ src/scenic/syntax/veneer.py
@@ -1645,7 +1645,9 @@ def alwaysProvidesOrientation(region):
     else:  # TODO improve somehow!
         try:
             sample = region.sample()
-            return sample.orientation is not None or sample is nowhere
+            result = sample.orientation is not None or sample is nowhere
+            print(f"ALWAYS PROVIDES ORIENTATION: {result}")
+            return result
         except RejectionException:
             return False
         except Exception as e:
