7529c3e3f5222df4d8cff33a11ef9aebd7f5a11d
Eric Vin <8935814+Eric-Vin@users.noreply.github.com>
2026-08-20 15:39:56 -0700

    Fix OpenScenarioXML Issues (#499)
    * Fix bugs from previous PR.
    
    * Additional fixes.

--- tests/core/test_serialization.py
+++ tests/core/test_serialization.py
@@ -73,7 +73,6 @@ class TestExportToScenicCode:
         assertSceneEquivalence(
             scene1,
             scene2,
-            ignoreDynamics=True,
             ignoreScenario=True,
         )
 
